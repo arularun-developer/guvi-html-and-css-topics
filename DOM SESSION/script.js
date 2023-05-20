463a77edf9683c98496cefb93e7dbd486c277b10
arularun-developer <arularunoffical1110@gmail.com>
2023-05-20 21:22:28 +0530

    guvi html and css

---
+++ DOM SESSION/script.js
@@ -0,0 +1,57 @@
+      // task1
+        // let heading = document.createElement('h1');
+        // heading.textContent ='Dynamic Heading';
+        // document.body.appendChild(heading);
+        //  create  a paragraph  element
+        // let paragraph = document.createElement('p');
+        // set the content of the paragraph element
+        // paragraph.textContent='this is a dyamivcally web site object paragraph..!';
+        // append the paragraph element to the body
+        // document.body.appendChild(paragraph);
+        // Mulitple element add to use append
+        // docuent.body.append(paragraph,heading)
+
+        // document.getElementById('firstDiv').append(heading,paragraph);
+
+
+        //creating anchor element
+        // let link =document.createElement('a');
+
+        // link.href = 'https://www.google.com';
+
+        // link.textContent = 'Visit googlecom';
+        // document.body.appendchild(link);
+
+        // document.getElementById('secondDiv').appendchild(link);
+
+
+    //     let link =document.createElement('a');
+    //     link.href ='https://www.google.com';
+    //     link.textContent = 'click me!'
+    //     document.getElementById('firstDiv').appendChild(link)
+          
+
+    //     let googlelogo =document.createElement('img');
+    //     googlelogo.setAttribute('src', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png')
+    //     googlelogo.setAttribute('alt','google logo');
+
+    //     googlelogo.width=200;
+    //     googlelogo.style.border ='2px double  red'
+    //     document.getElementById('secondDiv').appendChild(googlelogo)
+    
+    //   let listitems =['coffee' ,'tea','green','blacktea']
+
+
+// document.getElementById('firstDiv').innerHTML=
+// `<h1>coffee list</h1>
+// <ul>
+//     <li>black coffee</li>
+//     <li>cold coffee</li>
+//     <li>instant coffee</li>
+//     <li>filter coffee</li>
+// </ul>`
+
+
+
+
+
